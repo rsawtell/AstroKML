329beca51fda655553d3266b571916abcdd3e145
Reid <rsawtell@cs.wisc.edu>
2011-07-25 17:00:57 -0500

    version update

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup (name = 'AstroKML',
-       version = '1.0.0',
+       version = '1.0.1',
        author = 'Reid Sawtell',
        author_email='rwsawtel@mtu.edu',
        url='http://github.com/rsawtell/AstroKML',
